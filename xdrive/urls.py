5694c5b731a87791ecdf3ac6c72a6a0048814118
VishnuK24 <vishnu@labglo.com>
2020-01-03 16:02:10 +0530

    add image

--- xdrive/urls.py
+++ xdrive/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views import static
+
+
 
 from accounts.views import IndexView
 
@@ -28,5 +32,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 
     path('files/', include('files.urls')),
+
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT}),
+
+
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
